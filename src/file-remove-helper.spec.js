999af4fd643f1a6a658e95ffdda68e40cafa4834
Piotr Kowalski <piecioshka@gmail.com>
2023-10-08 11:16:16 +0200

    Remove unused files

--- src/file-remove-helper.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { removeAllPDFFiles } = require('./file-remove-helper');
-
-describe('FileRemoveHelper', () => {
-  it('removeAllPDFFiles', () => {
-    expect(typeof removeAllPDFFiles === 'function').toEqual(true);
-  });
-});
